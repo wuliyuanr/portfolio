66de0ae34873294995254d858590216e81bfd93b
wuliyuanr <168322914+wuliyuanr@users.noreply.github.com>
2025-12-23 19:50:57 +0800

    Delete portfolio: directory

--- portfolio:/script.js
+++
@@ -1,7 +0,0 @@
-const projects = document.querySelectorAll(".project");
-
-projects.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("active");
-    });
-});
